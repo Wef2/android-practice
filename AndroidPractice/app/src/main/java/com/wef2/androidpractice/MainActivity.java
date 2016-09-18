ddf910dcf90e319ff2e9e1ef67d5882da0deaf61
Wef2 <qhrud5725@hanmail.net>
2016-09-18 14:19:10 +0900

    Add Activities

--- AndroidPractice/app/src/main/java/com/wef2/androidpractice/MainActivity.java
+++ AndroidPractice/app/src/main/java/com/wef2/androidpractice/MainActivity.java
@@ -1,13 +1,35 @@
 package com.wef2.androidpractice;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button uploadButton, locationButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        uploadButton = (Button) findViewById(R.id.upload_button);
+        locationButton = (Button) findViewById(R.id.location_button);
+
+        uploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, UploadActivity.class));
+            }
+        });
+
+        locationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LocationActivity.class));
+            }
+        });
     }
 }
